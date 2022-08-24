6c12d9a916c9037d65806b5f2657606a40d25654
Kirtimaanwani <sushilsw111@gmail.com>
2022-08-24 16:44:46 +0530

    renaming

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # print(data)
 import pandas as pd
 from tkinter import *
-from Untitled import country_name
+from csv_modification import country_name
 rate = pd.read_csv("country_name.csv")
 
 window = Tk()
@@ -78,6 +78,53 @@ converted.place(x=580, y=400)
 
 calculate = Button(text="CONVERT", command=convert, bg="red", font=("Arial", 10, "bold"))
 calculate.place(x=460, y=410)
+
+
+# # ------------------------------FROM------------------------------------------
+# label_from = Label(text="FROM", font=("Arial", 25, "bold"))
+# label_from.grid(row=0, column=0)
+#
+# choice_from = None
+#
+#
+# def listbox_used_from(event):
+#     # Gets current selection from listbox
+#     global choice_from
+#     choice_from = listbox_from.get(listbox_from.curselection())
+#     # print("45852")
+#
+#
+# listbox_from = Listbox(height=4)
+# fruits = ["Apple", "Pear", "Orange", "Banana"]
+# for item in fruits:
+#     listbox_from.insert(fruits.index(item), item)
+# listbox_from.bind("<<ListboxSelect>>", listbox_used_from)
+# listbox_from.grid(row=1, column=0)
+#
+# label = Label(text="text", font=("Arial", 22, "bold"))
+# label.grid(row=0, column=2)
+# # -------------------------------TO---------------------------
+#
+# label_from = Label(text="TO", font=("Arial", 25, "bold"))
+# label_from.grid(row=0, column=4)
+#
+# choice_to = None
+#
+#
+# def listbox_used_to(event):
+#     # Gets current selection from listbox
+#     global choice_to
+#     choice_to = listbox_to.get(listbox_to.curselection())
+#
+#
+# listbox_to = Listbox(height=4)
+# fruits = ["Apple", "Pear", "Orange", "Banana"]
+# for item in fruits:
+#     listbox_to.insert(fruits.index(item), item)
+# listbox_to.bind("<<ListboxSelect>>", listbox_used_to)
+# listbox_to.grid(row=1, column=4)
+
+# -------------------------------------------------------------
 
 
 window.mainloop()
