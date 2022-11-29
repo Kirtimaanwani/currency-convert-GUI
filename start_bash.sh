00eebc9b044d1e33745f3d932dc751f5d4cb9f14
Kirtimaanwani <107944506+Kirtimaanwani@users.noreply.github.com>
2022-11-29 12:20:24 +0530

    Update start_bash.sh

--- start_bash.sh
+++ start_bash.sh
@@ -1,4 +1,18 @@
-conda create -p venv/ python=3.8 -y
+echo [$(date)]: "START"
+echo [$(date)]: "Creating conda venv with python version 3.8"
+conda create --prefix ./venv python==3.8 -y
 
-conda activate venv/
+echo [$(date)]: "activate venv"
+source activate ./venv
 
+echo [$(date)]: "Installing dev requirements"
+pip install -r requirements.txt
+
+
+echo [$(date)]: "getting currency rates"
+python run_this_first_to_get_latest_exchange_rates.py
+
+echo [$(date)]: "Run GUI"
+python main.py
+
+echo [$(date)]: "END"
